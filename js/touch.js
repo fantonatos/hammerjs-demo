8b8e6b4b887593cc5fb74e7b2c3463987febf46f
Fotis Antonatos <fotis.antonatos7@gmail.com>
2019-01-22 11:55:41 -0500

    Add support for pan and tripletap
    Up, Down, Left and Right, and three taps in quick sucession.

--- js/touch.js
+++ js/touch.js
@@ -9,6 +9,10 @@ var touch_interface = new Hammer.Manager(div_element);
     DOUBLETAP only works if added before SINGLETAP
 */
 touch_interface.add(new Hammer.Tap({
+    event: 'tripletap', taps: 3
+}))
+
+touch_interface.add(new Hammer.Tap({
     event: 'doubletap', taps: 2
 }) );
 
@@ -16,6 +20,13 @@ touch_interface.add(new Hammer.Tap({
     event: 'singletap'
 }) );
 
+touch_interface.add(new Hammer.Pan({
+    event: 'pan'
+}) );
+
+touch_interface.get('pan').set({ threshold: 50, direction: Hammer.DIRECTION_ALL });
+
+touch_interface.get('tripletap').recognizeWith('doubletap');
 
 // Inform Hammer.JS that double tap can follow a single tap
 touch_interface.get('doubletap').recognizeWith('singletap');
@@ -24,7 +35,27 @@ touch_interface.get('doubletap').recognizeWith('singletap');
 touch_interface.get('singletap').requireFailure('doubletap');
 
 // Touch Inteface Listeners
-touch_interface.on('singletap doubletap', function(ev)
+touch_interface.on('singletap doubletap tripletap', function(ev)
 {
     alert('You did a ' + ev.type);
+});
+
+touch_interface.on('panend', function(ev){
+    var direct = ev.direction;
+    switch(ev.direction){
+        case 8: // UP
+            direct = "up";
+            break;
+        case 16: // DOWN
+            direct = "down";
+            break;
+        case 4: // RIGHT
+            direct = "right";
+            break;
+        case 2: // LEFT
+            direct = "left";
+            break;
+
+    }
+    alert('You did a ' + direct);
 });
